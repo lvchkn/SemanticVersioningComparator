95fb1d74c8016ee44e4c9539b30d8316b95d351c
Aleksandr Levochkin <aleksandr.levochkin@rp.ru>
2021-08-05 21:50:50 +0300

    added negative jest tests

--- SemanticVersioningComparator/compareSemanticVersions.js
+++ SemanticVersioningComparator/compareSemanticVersions.js
@@ -1,6 +1,12 @@
 var SemVerCompare = function (leftOp, rightOp) {
+    var regExp = /^[0-9,.]*$/;
+    if (!regExp.test(leftOp) || !regExp.test(rightOp)) {
+        return "One of the strings contains non-numeric symbols!";
+    }
+    if (leftOp.length === 0 || rightOp.length === 0) {
+        return "One of the strings is empty!";
+    }
     var res = leftOp.localeCompare(rightOp, undefined, { numeric: true });
     return res > 0 ? 1 : res < 0 ? -1 : 0;
 };
-console.log(SemVerCompare("3.0", "2.2"));
 module.exports = SemVerCompare;
--- SemanticVersioningComparator/compareSemanticVersions.test.ts
+++ SemanticVersioningComparator/compareSemanticVersions.test.ts
@@ -14,7 +14,36 @@ test('compares semantic versions - positive scenario #3', () => {
     expect(semVerCompare("0.2001.199.14", "0.2001.199.14")).toBe(0);
 });
 
-test('compares semantic versions - positive scenario #3', () => {
+test('compares semantic versions - positive scenario #4', () => {
     expect(semVerCompare("133.20", "133.2.4")).toBe(1);
 });
 
+// Negative scenarios: 
+
+test('compares semantic versions - negative scenario #1', () => {
+    expect(semVerCompare(undefined, "2.2")).toBe("One of the strings contains non-numeric symbols!");
+});
+
+test('compares semantic versions - negative scenario #2', () => {
+    expect(semVerCompare(null, "2.2")).toBe("One of the strings contains non-numeric symbols!");
+});
+
+test('compares semantic versions - negative scenario #3', () => {
+    expect(semVerCompare("", "2.2")).toBe("One of the strings is empty!");
+});
+
+test('compares semantic versions - negative scenario #3', () => {
+    expect(semVerCompare("3", "")).toBe("One of the strings is empty!");
+});
+
+test('compares semantic versions - negative scenario #3', () => {
+    expect(semVerCompare("", "")).toBe("One of the strings is empty!");
+});
+
+test('compares semantic versions - negative scenario #4', () => {
+    expect(semVerCompare("wrawrawra", "2.2")).toBe("One of the strings contains non-numeric symbols!");
+});
+
+test('compares semantic versions - negative scenario #4', () => {
+    expect(semVerCompare("wrawrawra", undefined)).toBe("One of the strings contains non-numeric symbols!");
+});
